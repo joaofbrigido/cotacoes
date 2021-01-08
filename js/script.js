5164e390346e4c4ef5def81bff549630f39d7b9b
Joao Fernando <joaofbrigido@hotmail.com>
2021-01-07 22:02:27 -0300

    adicionada descrição das cotações e correções de animações

--- js/script.js
+++ js/script.js
@@ -2,30 +2,40 @@ function dinamicContent() {
     const containerInfo = document.querySelector('.info');
     const cotacoes = document.querySelectorAll('.cotacao');
     const infos = document.querySelectorAll('.info-content');
-    const placeHolder = document.querySelector('.place-holder');
     const activeClass = 'ativo';
+
+    // Começar com Animação e primeiro conteúdo.
     containerInfo.classList.add('anima');
+    infos[0].classList.add(activeClass);
+    cotacoes[0].classList.add(activeClass);
 
     function animaInfo() {
         containerInfo.classList.add('anima');
         setTimeout(() => {
             containerInfo.classList.remove('anima');
-        }, 410);
+        }, 310);
     }
     animaInfo();
+
+    function cotacaoAtiva(index) {
+        cotacoes.forEach((cotacao) => {
+            cotacao.classList.remove(activeClass);
+        });
+        cotacoes[index].classList.add(activeClass);
+    }
 
     function changeContent(index) {
         infos.forEach(info => {
             info.classList.remove(activeClass);
-        })
-        placeHolder.style.display = 'none';
+        });
         infos[index].classList.add(activeClass);
         animaInfo();
+        cotacaoAtiva(index);
     }
 
     cotacoes.forEach((cotacao, index) => {
         cotacao.addEventListener('click', () => changeContent(index));
-    })
+    });
 }
 
 dinamicContent();
